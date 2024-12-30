d9605381e2ec04b6402b3f65d0abbe90d8381e7e
Nillub <lubeynakom@gmail.com>
2024-12-30 15:27:19 +0300

    Added LibraryUtils.java and codes for token

---
+++ src/test/java/com/library/utilities/LibraryUtils.java
@@ -0,0 +1,79 @@
+package com.library.utilities;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LibraryUtils {
+    public static String getToken(String email,String password){
+
+        JsonPath jp = RestAssured.given().log().uri()
+                .accept(ContentType.JSON)
+                .contentType(ContentType.URLENC)
+                .formParam("email", email)
+                .formParam("password", password)
+
+                .when().post("/login")
+                .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .extract().jsonPath();
+
+        String accessToken = jp.getString("accessToken");
+
+        return accessToken;
+
+    }
+
+
+    public static String generateTokenByRole(String role) {
+
+        Map<String, String> roleCredentials = returnCredentials(role);
+        String email = roleCredentials.get("email");
+        String password = roleCredentials.get("password");
+
+        return  getToken(email,password);
+
+    }
+
+
+    public static Map<String, String> returnCredentials(String role) {
+        String email = "";
+        String password = "";
+
+        switch (role) {
+            case "librarian":
+                 email = ConfigurationReader.getProperty("librarian_username") ;
+                password = ConfigurationReader.getProperty("librarian_password") ;
+
+                break;
+
+            case "student":
+                email = ConfigurationReader.getProperty("student_username") ;
+                password = ConfigurationReader.getProperty("student_password");
+
+                break;
+
+
+            default:
+
+                throw new RuntimeException("Invalid Role Entry :\n>> " + role + " <<");
+        }
+        Map<String, String> credentials = new HashMap<>();
+        credentials.put("username", email);
+        credentials.put("password", password);
+
+        return credentials;
+
+    }
+
+
+
+
+
+
+
+}
